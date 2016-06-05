5f595e01572595299791f7c302419491862f76cc
Sergey Emelianov <ess_ii@outlook.com>
2016-06-05 21:43:06 +0300

    Initial Commit

---
+++ RANEPA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RANEPA
+//
+//  Created by Sergey Emelianov on 05/06/16.
+//  Copyright © 2016 Madb3er. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RANEPA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RANEPA
+//
+//  Created by Sergey Emelianov on 05/06/16.
+//  Copyright © 2016 Madb3er. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
